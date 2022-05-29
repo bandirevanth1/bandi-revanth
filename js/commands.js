4f954a94039d8c7523caa255b8b8d3db26d1b784
Bandi Revanth <raobm76@gmail.com>
2022-05-29 17:58:23 +0530

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,75 @@
+var password = "admin_is_bandi_revanth";
+var github = "https://github.com/bandirevanth/";
+var email = 'mailto:raobm76@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Bandi Revanth",
+  "I'm from Hyderabad, India. I design and code simple yet beautiful websites and programs.",
+  "I'm always interested trying out new things and building great projects.",
+  "I believe everything is an art when you put your consciousness in it.",
+  "I enjoy bringing new ideas to life! I am a self taught developer with a passion for learning and problem solving.",
+  "I'm also a self taught developer with a passion for learning and problem solving.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'github,         <a href="' + github + '" target="_blank">github/bandirevanth' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "View them on my GitHub.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Revanth?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          EMAIL!',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">&copy; Bandi Revanth. All rights reserved. [v2.1]</span>',
+  "8 888888888o          .8.          b.             8 8 888888888o.       8 8888  ",
+  "8 8888    `88.       .888.         888o.          8 8 8888    `^888.    8 8888  ",
+  "8 8888     `88      :88888.        Y88888o.       8 8 8888        `88.  8 8888  ",
+  "8 8888     ,88     . `88888.       .`Y888888o.    8 8 8888         `88  8 8888  ",
+  "8 8888.   ,88'    .8. `88888.      8o. `Y888888o. 8 8 8888          88  8 8888  ",
+  "8 8888888888     .8`8. `88888.     8`Y8o. `Y88888o8 8 8888          88  8 8888  ",
+  "8 8888    `88.  .8' `8. `88888.    8   `Y8o. `Y8888 8 8888         ,88  8 8888  ",
+  "8 8888      88 .8'   `8. `88888.   8      `Y8o. `Y8 8 8888        ,88'  8 8888  ",
+  "8 8888    ,88'.888888888. `88888.  8         `Y8o.` 8 8888    ,o88P'    8 8888  ",
+  "8 888888888P .8'       `8. `88888. 8            `Yo 8 888888888P'       8 8888  ",
+  "                                                                                ",
+  "8 888888888o.   8 8888888888 `8.`888b           ,8' .8.          b.             8 8888888 8888888888 8 8888        8 ",
+  "8 8888    `88.  8 8888        `8.`888b         ,8' .888.         888o.          8       8 8888       8 8888        8 ",
+  "8 8888     `88  8 8888         `8.`888b       ,8' :88888.        Y88888o.       8       8 8888       8 8888        8 ",
+  "8 8888     ,88  8 8888          `8.`888b     ,8' . `88888.       .`Y888888o.    8       8 8888       8 8888        8",
+  "8 8888.   ,88'  8 888888888888   `8.`888b   ,8' .8. `88888.      8o. `Y888888o. 8       8 8888       8 8888        8 ",
+  "8 888888888P'   8 8888            `8.`888b ,8' .8`8. `88888.     8`Y8o. `Y88888o8       8 8888       8 8888        8 ",
+  "8 8888`8b       8 8888             `8.`888b8' .8' `8. `88888.    8   `Y8o. `Y8888       8 8888       8 8888888888888 ",
+  "8 8888 `8b.     8 8888              `8.`888' .8'   `8. `88888.   8      `Y8o. `Y8       8 8888       8 8888        8 ",
+  "8 8888   `8b.   8 8888               `8.`8' .888888888. `88888.  8         `Y8o.`       8 8888       8 8888        8 ",
+  "8 8888     `88. 8 888888888888        `8.` .8'       `8. `88888. 8            `Yo       8 8888       8 8888        8 ",
+  "",
+  '<span class="color2">Welcome to my interactive web terminal, fellow humans and bots!</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+  "<span class=\"color2\">More features coming soon!</span>",
+];
